e34abc3f1f52b1d29c9cf23538714499a204ab44
Thomas Migus <thomas.migus@gmail.com>
2018-10-07 17:58:36 -0400

    Implementation of straight, straighflush, and flush added

--- handSwap.java
+++ handSwap.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 public class handSwap {
 	public boolean fullHouse(String s1, String s2, String s3, String s4, String s5)
 	{
@@ -45,16 +45,44 @@ public class handSwap {
 	
 	public boolean straight(String s1, String s2, String s3, String s4, String s5)
 	{
+		char[] rank = new char[5];
+		rank[0]=s1.charAt(1);
+		rank[1]=s2.charAt(1);
+		rank[2]=s3.charAt(1);
+		rank[3]=s4.charAt(1);
+		rank[4]=s5.charAt(1);
+		int match1=0;
+		Arrays.sort(rank);
+		for (int i=0;i<4;i++)
+		{
+			if (rank[i]!=rank[i+1]+1)
+				return false;
+		}
 		return true;
 	}
 	
 	public boolean straightFlush(String s1, String s2, String s3, String s4, String s5)
 	{
+		if (!straight(s1,s2,s3,s4,s5))
+			return false;
+		if (!flush(s1,s2,s3,s4,s5))
+			return false;
 		return true;
 	}
 	
 	public boolean flush(String s1, String s2, String s3, String s4, String s5)
 	{
+		char[] suit = new char[5];
+		suit[0]=s1.charAt(0);
+		suit[1]=s2.charAt(0);
+		suit[2]=s3.charAt(0);
+		suit[3]=s4.charAt(0);
+		suit[4]=s5.charAt(0);
+		for (int i=0;i<4;i++)
+		{
+			if (suit[i]!=suit[i+1])
+				return false;
+		}
 		return true;
 	}
 	
