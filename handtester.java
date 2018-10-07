8f7f97f15b5ca5df360e0cdf740eac3d2e4a839c
Thomas Migus <thomas.migus@gmail.com>
2018-10-07 16:30:17 -0400

    Additonal tests added

--- handtester.java
+++ handtester.java
@@ -6,7 +6,30 @@ public class handtester extends TestCase {
 	{
 		handSwap swapper = new handSwap();
 		
-		assertEquals(true, swapper.fullHouse(2,2));
+		assertEquals(true, swapper.fullHouse("C2","S2","H2","D5","C2"));
+		assertEquals(false, swapper.fullHouse("C2","S6","H2","D5","C2"));
 	}
-
+	
+	public void straightTester()
+	{
+		handSwap swapper = new handSwap();
+		assertEquals(true, swapper.straight("C2","S3","H4","D5","C6"));
+		assertEquals(false, swapper.straight("C2","S3","H4","D5","C7"));
+	}
+	
+	public void straightFlushTester()
+	{
+		handSwap swapper = new handSwap();
+		assertEquals(true, swapper.straightFlush("C2","C3","C4","C5","C6"));
+		assertEquals(false, swapper.straightFlush("C2","S3","H4","D5","C7"));
+		assertEquals(false, swapper.straightFlush("C2","S3","H4","D5","C6"));
+	}
+	
+	public void flushTester()
+	{
+		handSwap swapper = new handSwap();
+		assertEquals(true, swapper.straightFlush("C2","C3","C4","C5","C6"));
+		assertEquals(false, swapper.straightFlush("C2","S3","H4","D5","C7"));
+		assertEquals(false, swapper.straightFlush("C2","S3","H4","D5","C6"));
+	}
 }
